cae86d6fda41c28102dc8e316d378daf961b3d2f
adamb5441 <40124072+adamb5441@users.noreply.github.com>
2019-03-17 22:28:17 -0400

    Landing page

--- WebAPI/App_Start/RouteConfig.cs
+++ WebAPI/App_Start/RouteConfig.cs
@@ -15,17 +15,17 @@ namespace WebAPI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapHttpRoute(
-             name: "swagger",
-             routeTemplate: "",
-             defaults: null,
-             constraints: null,
-             handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
+            //routes.MapHttpRoute(
+            // name: "swagger",
+            // routeTemplate: "",
+            // defaults: null,
+            // constraints: null,
+            // handler: new RedirectHandler((message => message.RequestUri.ToString()), "swagger"));
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Swagger", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
